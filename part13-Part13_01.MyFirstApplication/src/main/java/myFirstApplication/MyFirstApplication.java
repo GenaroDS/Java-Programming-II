35425b4012a2fc3eb59dd9848054b426cf182505
GenaroDS <genarodistilio@gmail.com>
2023-01-27 22:43:04 -0300

    Graphical user interfaces exercisese completed

--- part13-Part13_01.MyFirstApplication/src/main/java/myFirstApplication/MyFirstApplication.java
+++ part13-Part13_01.MyFirstApplication/src/main/java/myFirstApplication/MyFirstApplication.java
@@ -1,11 +1,18 @@
 package myFirstApplication;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class MyFirstApplication {
-    
+public class MyFirstApplication extends Application {
+
+    @Override
+    public void start(Stage window) throws Exception{
+        window.setTitle("My first application");
+        window.show();
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(MyFirstApplication.class);
     }
 
 }
--- part13-Part13_05.TextStatistics/src/main/java/textstatistics/TextStatisticsApplication.java
+++ part13-Part13_05.TextStatistics/src/main/java/textstatistics/TextStatisticsApplication.java
@@ -1,11 +1,53 @@
 package textstatistics;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
-public class TextStatisticsApplication {
-
+public class TextStatisticsApplication extends Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(TextStatisticsApplication.class);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        BorderPane layout = new BorderPane();
+
+        HBox buttons = new HBox();
+        buttons.setSpacing(10);
+        buttons.getChildren().add(new Button("First"));
+        buttons.getChildren().add(new Button("Second"));
+        buttons.getChildren().add(new Button("Third"));
+
+        VBox texts = new VBox();
+        texts.setSpacing(10);
+        texts.getChildren().add(new Label("First"));
+        texts.getChildren().add(new Label("Second"));
+        texts.getChildren().add(new Label("Third"));
+
+        layout.setTop(buttons);
+        layout.setLeft(texts);
+        
+        HBox stats = new HBox();
+        stats.setSpacing(10);
+        stats.getChildren().add(new Label("Letters: 0"));
+        stats.getChildren().add(new Label("Words: 0"));
+        stats.getChildren().add(new Label("The longest word is:"));
+
+        layout.setCenter(new TextArea(""));
+        layout.setBottom(stats);
+
+        Scene view = new Scene(layout);
+
+        stage.setScene(view);
+        stage.show();
     }
 
 }
